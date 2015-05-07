020d38157d8d62e2f618924dfa7f37c85f627ce2
donhui <977675308@qq.com>
2015-05-07 10:18:32 +0800

    在开始备份前/备份完成后 执行svn update

--- auto_backup.py
+++ auto_backup.py
@@ -10,7 +10,7 @@ import logging
     2、批量将JENKINS_HOME_BAK_PATH中不在版本控制下的目录或文件添加到版本控制
     3、遍历JENKINS_HOME_BAK_PATH，如果其中的文件或目录不在JENKINS_HOME中，对其执行svn delete操作
     4、将JENKINS_HOME_BAK_PATH提交到SVN
-    使用到的svn相关命令有：status、add、delete、ci
+    使用到的svn相关命令有：status、add、delete、commit、update
 '''
 
 
@@ -28,18 +28,19 @@ def main():
                 JENKINS_HOME_BAK_PATH = tmp[1].strip()
             else:
                 pass
-
-    # 1、调用ant命令copy JENKINS_HOME_PATH to JENKINS_HOME_BAK_PATH
+    # 1、在JENKINS_HOME_BAK_PATH执行svn update命令
+    svncli.update(JENKINS_HOME_BAK_PATH)
+    # 2、调用ant命令copy JENKINS_HOME_PATH to JENKINS_HOME_BAK_PATH
     os.system("ant -f backup.xml")
 
     # svn_status_file用来存储svn status命令的输出结果
     svn_status_file = "svn_status.txt"
 
-    # 2、批量将JENKINS_HOME_BAK_PATH中不在版本控制下的目录或文件添加到版本控制
-    # 2.1执行svn status命令，将输出结果定向到svn_status.txt中
+    # 3、批量将JENKINS_HOME_BAK_PATH中不在版本控制下的目录或文件添加到版本控制
+    # 3.1执行svn status命令，将输出结果定向到svn_status.txt中
     svncli.status(JENKINS_HOME_BAK_PATH, svn_status_file)
 
-    # 2.2读取并解析svn_status.txt，将不在版本控制下的目录或文件添加到版本控制，未在版本控制下的文件或目录所在的行会以“?”开头
+    # 3.2读取并解析svn_status.txt，将不在版本控制下的目录或文件添加到版本控制，未在版本控制下的文件或目录所在的行会以“?”开头
     with open(svn_status_file, "r") as f:
         for line in f.readlines():
             if line.startswith("?"):
@@ -48,9 +49,9 @@ def main():
                 # 将不在版本控制下的目录或文件添加到版本控制
                 svncli.add(line)
 
-    # 3、遍历JENKINS_HOME_BAK_PATH，如果其中的文件或目录不在JENKINS_HOME中，对其执行svn delete操作
+    # 4、遍历JENKINS_HOME_BAK_PATH，如果其中的文件或目录不在JENKINS_HOME中，对其执行svn delete操作
     for dirpath, dirs, files in os.walk(JENKINS_HOME_BAK_PATH):
-        # 3.1、遍历JENKINS_HOME_BAK_PATH，如果其中的文件不在JENKINS_HOME中，对其执行svn delete操作
+        # 4.1、遍历JENKINS_HOME_BAK_PATH，如果其中的文件不在JENKINS_HOME中，对其执行svn delete操作
         for f in files:
             # JENKINS_HOME_BAK_PATH下的文件
             bakfile = os.path.join(dirpath, f)
@@ -64,7 +65,7 @@ def main():
                     # 对不在JENKINS_HOME_PATH下的文件执行svn delete操作
                     logging.debug(orginfile + " is not exists.")
                     svncli.delete_from_wc(bakfile)
-        # 3.2、遍历JENKINS_HOME_BAK_PATH，如果其中的目录不在JENKINS_HOME中，对其执行svn delete操作
+        # 4.2、遍历JENKINS_HOME_BAK_PATH，如果其中的目录不在JENKINS_HOME中，对其执行svn delete操作
         for bak_dir in dirs:
             # JENKINS_HOME_BAK_PATH下的目录
             bakdir = os.path.join(dirpath, bak_dir)
@@ -78,9 +79,12 @@ def main():
                     # 对不在JENKINS_HOME_PATH下的目录执行svn delete操作
                     svncli.delete_from_wc(bakdir)
 
-    # 4、将JENKINS_HOME_BAK_PATH提交到SVN
+    # 5、将JENKINS_HOME_BAK_PATH提交到SVN
     log_message = "daily backup JENKINS_JOME"
     svncli.commit(JENKINS_HOME_BAK_PATH, log_message)
+
+    # 6、在JENKINS_HOME_BAK_PATH执行svn update命令
+    svncli.update(JENKINS_HOME_BAK_PATH)
 
 if __name__ == "__main__":
     main()
