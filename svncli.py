e669453931476be4b3d377d194f0eb3f3810e727
donhui <977675308@qq.com>
2015-06-25 15:18:29 +0800

    svn 命令 传递的参数用""包含起来

--- svncli.py
+++ svncli.py
@@ -12,7 +12,7 @@ logger.setLevel(logging.DEBUG)
 
 # merge function
 def merge(src, dst):
-    merge_command = 'svn merge -r 1:HEAD %s %s' % (src, dst)
+    merge_command = 'svn merge -r 1:HEAD "%s" "%s"' % (src, dst)
     logging.debug(merge_command)
     os.system(merge_command)
     return
@@ -20,7 +20,7 @@ def merge(src, dst):
 
 # copy function
 def copy(src, dst):
-    copy_command = 'svn copy %s %s' % (src, dst)
+    copy_command = 'svn copy "%s" "%s"' % (src, dst)
     logging.debug(copy_command)
     os.system(copy_command)
     return
@@ -29,7 +29,7 @@ def copy(src, dst):
 # update function
 # wc_dir:the dir of working copy
 def update(wc_dir):
-    update_command = 'svn up %s' % wc_dir
+    update_command = 'svn up "%s"' % wc_dir
     logging.debug(update_command)
     os.system(update_command)
     return
@@ -38,7 +38,7 @@ def update(wc_dir):
 # commit function
 # wc_dir:the dir of working copy
 def commit(wc_dir, log_message):
-    commit_command = 'svn ci -m "%s" %s' % (log_message, wc_dir)
+    commit_command = 'svn ci -m "%s" "%s"' % (log_message, wc_dir)
     logging.debug(commit_command)
     os.system(commit_command)
     return
@@ -46,7 +46,7 @@ def commit(wc_dir, log_message):
 
 # add function
 def add(path):
-    add_command = 'svn add %s' % path
+    add_command = 'svn add "%s"' % path
     logging.debug(add_command)
     os.system(add_command)
     return
@@ -56,7 +56,7 @@ def add(path):
 # wc_dir:the dir of working copy
 # output_file: to store the output of svn st command
 def status(wc_dir, output_file):
-    status_command = 'svn st %s > %s' % (wc_dir, output_file)
+    status_command = 'svn st "%s" > "%s"' % (wc_dir, output_file)
     logging.debug(status_command)
     os.system(status_command)
     return
@@ -64,7 +64,7 @@ def status(wc_dir, output_file):
 
 # delete_from_wc function : delete from path (woking copy)
 def delete_from_wc(path):
-    delete_command = 'svn delete %s' % path
+    delete_command = 'svn delete "%s"' % path
     logging.debug(delete_command)
     os.system(delete_command)
     return
@@ -72,7 +72,7 @@ def delete_from_wc(path):
 
 # delete_from_repo function : delete from url (repository)
 def delete_from_repo(url, log_message):
-    delete_command = 'svn delete -m "%s" %s' % (log_message, url)
+    delete_command = 'svn delete -m "%s" "%s"' % (log_message, url)
     logging.debug(delete_command)
     os.system(delete_command)
     return
